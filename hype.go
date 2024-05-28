0f63a94830e0d8a629e43705c70614def8a9fc53
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-05-28 13:29:16 +0200

    Use a better lexer selection mechanism

--- hype.go
+++ hype.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/alecthomas/chroma/v2/lexers"
 	"github.com/alecthomas/chroma/v2/quick"
@@ -35,18 +36,24 @@ func main() {
 	code := string(binary)
 
 	// Find out its lexer
-	lexer := lexers.Analyse(code)
+	lexer := lexers.Match(os.Args[1])
 	if lexer == nil {
-		lexer = lexers.Fallback
+		// Use the fallback lexer? Not sure - it is likely to make a mess of the output
+		//lexer = lexers.Fallback
+
+		// Instead, as we don't understand the filetype, output the file as plain text
+		os.Stdout.WriteString(code)
+		return
 	}
 
-	// Load the config
+	// Load the config, looking for a config file in the install location, the home dir and the current location
 	viper.SetConfigName(".hype")
 	viper.SetConfigType("yaml")
+	viper.AddConfigPath(filepath.Dir(os.Args[0]))
 	viper.AddConfigPath("$HOME")
+	viper.AddConfigPath(".")
 	viper.SetDefault("Formatter", "terminal16m")
 	viper.SetDefault("Style", "github-dark")
-	viper.AddConfigPath(".")
 	err = viper.ReadInConfig()
 
 	// If there's an error or the config doesn't exist, use some defaults
@@ -57,6 +64,11 @@ func main() {
 	// Process the configuration
 	config = new(Config)
 	viper.Unmarshal(config)
+
+	// Include "fmt" to use these debug statements
+	// fmt.Printf("lexer: %s\n", lexer.Config().Name)
+	// fmt.Printf("formatter: %s\n", config.Formatter)
+	// fmt.Printf("style: %s\n", config.Style)
 
 	// Print the file through the highlighter
 	err = quick.Highlight(os.Stdout, code, lexer.Config().Name, config.Formatter, config.Style)
